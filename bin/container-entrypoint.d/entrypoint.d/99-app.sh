3272e2ba941c55c6332dbfc90803beb36786310a
Sébastian Molle <sebastian.molle@gmail.com>
2024-12-26 20:56:06 +0100

    Load app entrypoint.d at startup

--- bin/container-entrypoint.d/entrypoint.d/99-app.sh
+++ bin/container-entrypoint.d/entrypoint.d/99-app.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -d /app/bin/container-entrypoint.d/entrypoint.d ]; then
+
+  for FILE in $(find /app/bin/container-entrypoint.d/entrypoint.d -iname \*.sh | sort); do
+    source ${FILE}
+  done
+
+fi
+
 true
